c3a9dfd11028a3375f31373dbe94d30b1297f455
Victor Alvarez <VPALVAREZ@puce.edu.ec>
2025-11-14 15:22:09 -0500

    Inicio laboratorio final

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,30 +1,66 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
 import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var reposAdapter: ReposAdapter
+
+    private val reposAdapter = ReposAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupRecyclerView()
+        fetchRepositories()
     }
 
-    private fun setupRecyclerView(){
-        reposAdapter = ReposAdapter()
+    private fun setupRecyclerView() {
         binding.repoRecyclerView.adapter = reposAdapter
-
-
     }
 
+    private fun fetchRepositories() {
+        val apiService = RetrofitClient.gitHubApiService
+        val call = apiService.getRepos()
 
+        call.enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                if (response.isSuccessful) {
+                    val repos = response.body()
+                    if (repos != null && repos.isNotEmpty()) {
+                        reposAdapter.updateRepositories(repos)
+                    } else {
+                        showMessage(msg = "Usted no tiene repositorios")
+                    }
+                } else {
+                    val errorMsg = when (response.code()) {
+                        401 -> "No autorizado"
+                        403 -> "Prohibido"
+                        404 -> "No encontrado"
+                        else -> "Error: ${response.code()}"
+                    }
+                    Log.e("MainActivity", "Error: $errorMsg")
+                    showMessage(msg = "Error: $errorMsg")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                showMessage(msg = "Error: Error de Conexión")
+                Log.e("MainActivity", "Error: ${t.message}")
+            }
+        })
+    }
+
+    private fun showMessage(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
 }
